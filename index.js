68a91aca66052ee3183e955010b366fa2e4a55a9
Raul <eduardoct120@gmail.com>
2017-04-10 22:42:52 -0500

    DB connection implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require( 'express' )
 const bodyParser = require( 'body-parser' )
+const mongoose = require( 'mongoose' )
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,6 +10,35 @@ const port = process.env.PORT || 3000
 app.use( bodyParser.urlencoded( {extended: false} ) )
 app.use( bodyParser.json() )
 
-app.listen( port, () => {
-  console.log( `Running on port ${port}` )
+app.get( '/api/product', ( req, res ) => {
+  res.status( 200 ).send( {products: []} )
+} )
+
+app.get( '/api/product/:productId', ( req, res ) => {
+
+} )
+
+app.post( '/api/product', ( req, res ) => {
+  console.log( req.body )
+  res.status( 200 ).send( {message: 'product received'} )
+} )
+
+app.put( '/api/product/:productId', ( req, res ) => {
+
+} )
+
+app.delete( '/api/product/:productId', ( req, res ) => {
+
+} )
+
+mongoose.connect( 'mongodb://localhost:27017/shop', ( err, res ) => {
+  if( err ) {
+    return console.log( `DB connection erro : ${err}` )
+  }
+
+  console.log( 'DB Connection enabled..' )
+
+  app.listen( port, () => {
+    console.log( `Running on port ${port}` )
+  } )
 } )
