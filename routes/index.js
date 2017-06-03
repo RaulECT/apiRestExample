d226578ef0468c39b7b41bfdfe2722ed84e5bce2
Raul Canul <eduardoct120@gmail.com>
2017-06-03 15:24:26 -0500

    Middleware implemented

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@
 
 const express = require( 'express' )
 const ProductCtrl = require( '../controllers/product' )
+const userCtrl = require('../controllers/user')
 const api = express.Router()
+const auth = require('../middlewares/auth');
 
 api.get( '/product', ProductCtrl.getProducts )
 api.get( '/product/:productId', ProductCtrl.getProduct )
 api.post( '/product', ProductCtrl.saveProduct )
 api.put( '/product/:productId', ProductCtrl.updateProducts )
 api.delete( '/product/:productId', ProductCtrl.deleteProduct )
+api.post( '/signup', userCtrl.signUp )
+api.post( '/signin', userCtrl.signIn )
+api.get('/private', auth, (req, res) => {
+  res.status(200).send({message: 'Tienes acceso'})
+})
 
 module.exports = api
